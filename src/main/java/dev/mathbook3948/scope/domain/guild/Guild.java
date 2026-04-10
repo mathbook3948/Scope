9152f04eb16a13244e5555e752d37934a419d2fd
mathbook3948 <wjddbsgh58@gmail.com>
2026-04-11 01:06:53 +0900

    modify: Guild Persistable 구현하게 수정

--- src/main/java/dev/mathbook3948/scope/domain/guild/Guild.java
+++ src/main/java/dev/mathbook3948/scope/domain/guild/Guild.java
@@ -3,7 +3,10 @@ package dev.mathbook3948.scope.domain.guild;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.PostLoad;
+import jakarta.persistence.PostPersist;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,12 +15,16 @@ import java.time.Instant;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.domain.Persistable;
 
 @Entity
 @Table(name = "t_scp_guild")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Guild {
+public class Guild implements Persistable<Long>{
+
+    @Transient
+    private boolean isNew = true;
 
     @Id
     @Column(name = "guild_id")
@@ -33,6 +40,16 @@ public class Guild {
     @UpdateTimestamp
     @Column(name = "updated_at", nullable = false, updatable = true)
     private Instant updatedAt;
+
+    @Override
+    public Long getId() { return this.guildId; }
+
+    @Override
+    public boolean isNew() { return isNew; }
+
+    @PostPersist
+    @PostLoad
+    void markNotNew() { this.isNew = false; }
 
     public static Guild of(Long guildId, String name) {
         Guild guild = new Guild();
