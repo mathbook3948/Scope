572fc12deddfa3cb5865130d76b0a9dc2055d547
mathbook3948 <wjddbsgh58@gmail.com>
2026-04-11 16:16:30 +0900

    refactor: 후 API 서버 개발을 위해 core, bot 프로젝트 분리

--- scope-bot/src/test/java/dev/mathbook3948/scope/ScopeApplicationTests.java
+++ scope-bot/src/test/java/dev/mathbook3948/scope/ScopeApplicationTests.java
@@ -1,13 +1,8 @@
 package dev.mathbook3948.scope;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ScopeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
