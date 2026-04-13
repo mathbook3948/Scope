9654040cdc18737d6dc620ee536fabd668dbb967
mathbook3948 <wjddbsgh58@gmail.com>
2026-04-13 22:49:20 +0900

    refactor: 메시지 이벤트 JDA 도메인 변환을 JdaMapper로 이동

--- scope-bot/src/main/java/dev/mathbook3948/scope/discord/listener/GuildMessageEventListener.java
+++ scope-bot/src/main/java/dev/mathbook3948/scope/discord/listener/GuildMessageEventListener.java
@@ -1,14 +1,8 @@
 package dev.mathbook3948.scope.discord.listener;
 
-import dev.mathbook3948.scope.domain.guild.AuthorType;
-import dev.mathbook3948.scope.domain.guild.message.GuildMessageEventInfo;
-import dev.mathbook3948.scope.domain.guild.message.GuildMessageSourceType;
+import dev.mathbook3948.scope.discord.utils.JdaMapper;
 import dev.mathbook3948.scope.facade.GuildMessageEventFacade;
 import lombok.RequiredArgsConstructor;
-import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.entities.channel.ChannelType;
-import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
-import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
@@ -24,61 +18,18 @@ public class GuildMessageEventListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (!event.isFromGuild()) return;
-
-        String content = event.getMessage().getContentRaw();
-        User author = event.getAuthor();
-        GuildMessageEventInfo info = new GuildMessageEventInfo(
-            event.getGuild().getIdLong(),
-            event.getChannel().getIdLong(),
-            author.getIdLong(),
-            event.getMessageIdLong(),
-            content.codePointCount(0, content.length()),
-            sourceTypeOf(event.getChannel()),
-            AuthorType.from(author.isBot(), author.isSystem())
-        );
-        guildMessageEventFacade.onMessageSend(info);
+        guildMessageEventFacade.onMessageSend(JdaMapper.toMessageEventInfo(event));
     }
 
     @Override
     public void onMessageDelete(MessageDeleteEvent event) {
         if (!event.isFromGuild()) return;
-
-        GuildMessageEventInfo info = new GuildMessageEventInfo(
-            event.getGuild().getIdLong(),
-            event.getChannel().getIdLong(),
-            null,
-            event.getMessageIdLong(),
-            null,
-            sourceTypeOf(event.getChannel()),
-            AuthorType.UNKNOWN
-        );
-        guildMessageEventFacade.onMessageDelete(info);
+        guildMessageEventFacade.onMessageDelete(JdaMapper.toMessageDeleteInfo(event));
     }
 
     @Override
     public void onMessageUpdate(MessageUpdateEvent event) {
         if (!event.isFromGuild()) return;
-
-        String content = event.getMessage().getContentRaw();
-        User author = event.getAuthor();
-        GuildMessageEventInfo info = new GuildMessageEventInfo(
-            event.getGuild().getIdLong(),
-            event.getChannel().getIdLong(),
-            author.getIdLong(),
-            event.getMessageIdLong(),
-            content.codePointCount(0, content.length()),
-            sourceTypeOf(event.getChannel()),
-            AuthorType.from(author.isBot(), author.isSystem())
-        );
-        guildMessageEventFacade.onMessageUpdate(info);
-    }
-
-    private static GuildMessageSourceType sourceTypeOf(MessageChannel channel) {
-        if (channel instanceof ThreadChannel thread) {
-            return thread.getParentChannel().getType() == ChannelType.FORUM
-                ? GuildMessageSourceType.FORUM
-                : GuildMessageSourceType.THREAD;
-        }
-        return GuildMessageSourceType.CHANNEL;
+        guildMessageEventFacade.onMessageUpdate(JdaMapper.toMessageEventInfo(event));
     }
 }
--- scope-bot/src/main/java/dev/mathbook3948/scope/discord/utils/JdaMapper.java
+++ scope-bot/src/main/java/dev/mathbook3948/scope/discord/utils/JdaMapper.java
@@ -3,6 +3,8 @@ package dev.mathbook3948.scope.discord.utils;
 import dev.mathbook3948.scope.domain.guild.AuthorType;
 import dev.mathbook3948.scope.domain.guild.channel.GuildChannelInfo;
 import dev.mathbook3948.scope.domain.guild.channel.GuildChannelType;
+import dev.mathbook3948.scope.domain.guild.message.GuildMessageEventInfo;
+import dev.mathbook3948.scope.domain.guild.message.GuildMessageSourceType;
 import dev.mathbook3948.scope.domain.guild.reaction.GuildReactionEventInfo;
 import dev.mathbook3948.scope.domain.guild.thread.GuildThreadEventInfo;
 import lombok.AccessLevel;
@@ -10,9 +12,14 @@ import lombok.NoArgsConstructor;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.Channel;
 
+import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.entities.channel.attribute.ICategorizableChannel;
 import net.dv8tion.jda.api.entities.channel.attribute.IPositionableChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.api.events.message.react.GenericMessageReactionEvent;
 
 /**
@@ -61,6 +68,63 @@ public class JdaMapper {
             event.getReaction().getEmoji().getAsReactionCode(),
             authorType
         );
+    }
+
+    public static GuildMessageEventInfo toMessageEventInfo(MessageReceivedEvent event) {
+        String content = event.getMessage().getContentRaw();
+        User author = event.getAuthor();
+        return new GuildMessageEventInfo(
+            event.getGuild().getIdLong(),
+            event.getChannel().getIdLong(),
+            author.getIdLong(),
+            event.getMessageIdLong(),
+            content.codePointCount(0, content.length()),
+            sourceTypeOf(event.getChannel()),
+            AuthorType.from(author.isBot(), author.isSystem())
+        );
+    }
+
+    public static GuildMessageEventInfo toMessageEventInfo(MessageUpdateEvent event) {
+        String content = event.getMessage().getContentRaw();
+        User author = event.getAuthor();
+        return new GuildMessageEventInfo(
+            event.getGuild().getIdLong(),
+            event.getChannel().getIdLong(),
+            author.getIdLong(),
+            event.getMessageIdLong(),
+            content.codePointCount(0, content.length()),
+            sourceTypeOf(event.getChannel()),
+            AuthorType.from(author.isBot(), author.isSystem())
+        );
+    }
+
+    public static GuildMessageEventInfo toMessageDeleteInfo(MessageDeleteEvent event) {
+        return new GuildMessageEventInfo(
+            event.getGuild().getIdLong(),
+            event.getChannel().getIdLong(),
+            null,
+            event.getMessageIdLong(),
+            null,
+            sourceTypeOf(event.getChannel()),
+            AuthorType.UNKNOWN
+        );
+    }
+
+    /**
+     * 메시지 채널의 출처 타입을 판별한다.
+     * <p>포럼 포스트도 JDA에서는 {@link ThreadChannel}이므로, 부모가 {@link ChannelType#FORUM}인지
+     * 확인해 {@code FORUM}과 일반 {@code THREAD}를 구분한다. 미디어 채널 포스트는 현재 {@code THREAD}로 분류된다.
+     */
+    private static GuildMessageSourceType sourceTypeOf(MessageChannel channel) {
+        if (channel instanceof ThreadChannel thread) {
+            return thread.getParentChannel().getType() == ChannelType.FORUM
+                ? GuildMessageSourceType.FORUM
+                : GuildMessageSourceType.THREAD;
+        }
+
+        //TODO MEDIA도 처리필요
+
+        return GuildMessageSourceType.CHANNEL;
     }
 
     public static GuildThreadEventInfo toThreadEventInfo(ThreadChannel thread) {
